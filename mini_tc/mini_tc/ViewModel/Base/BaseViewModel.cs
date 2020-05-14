adcb2633daa56e602b6ab1e4932c540c582cced0
Piotr Sobieszczyk <p.sobieszczyk1@gmail.com>
2020-05-14 20:01:54 +0200

    base

---
+++ mini_tc/mini_tc/ViewModel/Base/BaseViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace Menu.ViewModel
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Zdarzenie uruchamiane, gdy dowolna właściwość zmienia swoją wartość
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void onPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
